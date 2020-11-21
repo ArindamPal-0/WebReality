b13f5d29d34a373e4f319e3f9a186d48d12fe2c9
Arindam Pal <arindampal2000@gmail.com>
2020-11-21 12:03:05 +0530

    Added html pages for github pages

--- public/index.js
+++ public/index.js
@@ -3,7 +3,8 @@ console.log('hello world!');
 routes.forEach(route => {
     const div = document.createElement('div');
     const a = document.createElement('a')
-    a.href = `${url}/${route}`;
+    a.href = `${url}/${route}${ext}`;
+    console.log(a.href);
     a.textContent = route;
     div.appendChild(a);
     document.body.appendChild(div);
